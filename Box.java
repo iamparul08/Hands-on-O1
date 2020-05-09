1f0540783f666f0f3e8b319ada8bab9c1892ff3b
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-09 12:43:15 +0530

    Update Box.java

--- Box.java
+++ Box.java
@@ -1,4 +1,4 @@
-/* Create a class Box that uses a parameterized constructor to initialize the dimensions of a box.The dimensions of the Box are width, height, depth. The class should have a method that can return the volume of the box. Create an object of the Box class and test the functionalities.*/
+
 
 class Box {
 	double h,w,d;	
